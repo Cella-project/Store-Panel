fe814749ad006ea009813ea8c6708a967a99718a
ZiadAssal <zead142684@eng.psu.edu.eg>
2023-06-17 18:50:19 +0300

    sidebar

--- src/components/global/SideBar.js
+++ src/components/global/SideBar.js
@@ -22,40 +22,40 @@ const SideBar = () => {
     <div className={`side-bar ${language === 'ar' ? 'side-bar-arabic' : ''} flex-col-center orange-bg`}>
       <PerfectScrollbar className="side-bar--cont flex-col-top-start">
         <NavLink end to={'/'}>
-          <span className={`side-bar--cont--description inter size-20px font-light ${mode === 'dark-mode' ? 'gray' : 'white'}`}>{translations.home}</span>
           <i className={`bi bi-house-door margin-8px-V size-24px ${mode === 'dark-mode' ? 'gray' : 'white'} size-24px`} />
+          <span className={`side-bar--cont--description inter size-20px font-light ${mode === 'dark-mode' ? 'gray' : 'white'}`}>{translations.home}</span>
         </NavLink>
         <NavLink to={'/products'} >
-          <span className={`side-bar--cont--description inter size-20px font-light ${mode === 'dark-mode' ? 'gray' : 'white'}`}>{translations.products}</span>
           <i className={`bi bi-box-seam margin-8px-V ${mode === 'dark-mode' ? 'gray' : 'white'} size-24px`} />
+          <span className={`side-bar--cont--description inter size-20px font-light ${mode === 'dark-mode' ? 'gray' : 'white'}`}>{translations.products}</span>
         </NavLink>
         {/* <NavLink to={'/orders'} >
-          <span className={`side-bar--cont--description inter size-20px font-light ${mode === 'dark-mode' ? 'gray' : 'white'}`}>Orders</span>
           <i className={`bi bi-receipt margin-8px-V ${mode === 'dark-mode' ? 'gray' : 'white'} size-24px`} />
+          <span className={`side-bar--cont--description inter size-20px font-light ${mode === 'dark-mode' ? 'gray' : 'white'}`}>Orders</span>
         </NavLink> */}
         <NavLink to={'/ordersHistory'} >
-          <span className={`side-bar--cont--description inter size-20px font-light ${mode === 'dark-mode' ? 'gray' : 'white'}`}>{translations.orderHistory}</span>
           <i className={`bi bi-hourglass-split margin-8px-V ${mode === 'dark-mode' ? 'gray' : 'white'} size-24px`} />
+          <span className={`side-bar--cont--description inter size-20px font-light ${mode === 'dark-mode' ? 'gray' : 'white'}`}>{translations.orderHistory}</span>
         </NavLink>
         <NavLink to={'/reviews'} >
-          <span className={`side-bar--cont--description inter size-20px font-light ${mode === 'dark-mode' ? 'gray' : 'white'}`}>{translations.reviews}</span>
           <i className={`bi bi-stars margin-8px-V ${mode === 'dark-mode' ? 'gray' : 'white'} size-24px`} />
+          <span className={`side-bar--cont--description inter size-20px font-light ${mode === 'dark-mode' ? 'gray' : 'white'}`}>{translations.reviews}</span>
         </NavLink>
         <NavLink to={'/vouchers'} >
-          <span className={`side-bar--cont--description inter size-20px font-light ${mode === 'dark-mode' ? 'gray' : 'white'}`}>{translations.vouchers}</span>
           <i className={`bi bi-tags margin-8px-V ${mode === 'dark-mode' ? 'gray' : 'white'} size-24px`} />
+          <span className={`side-bar--cont--description inter size-20px font-light ${mode === 'dark-mode' ? 'gray' : 'white'}`}>{translations.vouchers}</span>
         </NavLink>
         <NavLink to={'/logActivities'} >
-          <span className={`side-bar--cont--description inter size-20px font-light ${mode === 'dark-mode' ? 'gray' : 'white'}`}>{translations.logActivity}</span>
           <i className={`bi bi-clock-history margin-8px-V ${mode === 'dark-mode' ? 'gray' : 'white'} size-24px`} />
+          <span className={`side-bar--cont--description inter size-20px font-light ${mode === 'dark-mode' ? 'gray' : 'white'}`}>{translations.logActivity}</span>
         </NavLink>
         <NavLink to={'/comp&suggestions'} >
-          <span className={`side-bar--cont--description inter size-20px font-light ${mode === 'dark-mode' ? 'gray' : 'white'}`}>{translations.compSuggestions}</span>
           <i className={`bi bi-exclamation-triangle margin-8px-V ${mode === 'dark-mode' ? 'gray' : 'white'} size-24px`} />
+          <span className={`side-bar--cont--description inter size-20px font-light ${mode === 'dark-mode' ? 'gray' : 'white'}`}>{translations.compSuggestions}</span>
         </NavLink>
         <NavLink onClick={handleLogout} to={'/auth/login'} >
-          <span className={`side-bar--cont--description inter size-20px font-light ${mode === 'dark-mode' ? 'gray' : 'white'}`}>{translations.logout}</span>
           <i className={`bi bi-door-open margin-8px-V ${mode === 'dark-mode' ? 'gray' : 'white'} size-24px`} />
+          <span className={`side-bar--cont--description inter size-20px font-light ${mode === 'dark-mode' ? 'gray' : 'white'}`}>{translations.logout}</span>
         </NavLink>
       </PerfectScrollbar>
     </div>
